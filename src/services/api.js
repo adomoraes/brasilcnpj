38c93cca9eea017f2262cae663ec44c03a45b1f2
Eduardo Moraes <adomoraes@gmail.com>
2024-07-28 08:06:05 -0300

    feat: feat: integrate API service for CNPJ search in SearchBar component
    - Created src/services/api.js with fetchCnpjData function to fetch CNPJ data from the API.
    - Updated SearchBar component to use fetchCnpjData for fetching CNPJ data.
    - Added logic to clean CNPJ input by removing non-numeric characters before API call.
    - Display company details (email, bairro, numero, municipio, logradouro, razao_social, nome_fantasia, ddd_telefone_1, descricao_situacao_cadastral, data_inicio_atividade, cnae_fiscal_descricao) in the SearchBar component.

--- src/services/api.js
+++ src/services/api.js
@@ -5,5 +5,34 @@ export const fetchCnpjData = async (cnpj) => {
 	const response = await axios.get(
 		`https://brasilapi.com.br/api/cnpj/v1/${cleanedCnpj}`
 	)
-	return response.data
+	if (response.status !== 200) {
+		throw new Error("Erro ao buscar dados do CNPJ")
+	}
+
+	const data = response.data
+
+	// ▪ Nome
+	// ▪ Razão Social
+	// ▪ Data de Abertura
+	// ▪ Situação
+	// ▪ Atividade Principal
+	// ▪ Endereço Completo
+	// ▪ Telefone
+	// ▪ E-mail
+
+	const companyData = {
+		razao_social: data.razao_social,
+		nome_fantasia: data.nome_fantasia,
+		data_inicio_atividade: data.data_inicio_atividade,
+		descricao_situacao_cadastral: data.descricao_situacao_cadastral,
+		cnae_fiscal_descricao: data.cnae_fiscal_descricao,
+		bairro: data.bairro,
+		numero: data.numero,
+		municipio: data.municipio,
+		logradouro: data.logradouro,
+		ddd_telefone_1: data.ddd_telefone_1,
+		email: data.email,
+	}
+
+	return companyData
 }
